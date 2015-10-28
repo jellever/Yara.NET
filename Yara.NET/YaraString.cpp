e4332464d943285ea54136190311f15b84976fa0
Jelle Vergeer <jelle@jellevergeer.com>
2015-10-28 19:42:25 +0100

    - Fixed absolute->relative paths

--- Yara.NET/YaraString.cpp
+++ Yara.NET/YaraString.cpp
@@ -7,8 +7,8 @@ namespace YaraNET
 		this->offset = offset;
 		this->memoryAddress = memoryAddress;
 		this->identifierName = identifier;
-		this->data = gcnew array<Byte>(dataSize.ToInt64());
 		Int32 dataSize32 = Convert::ToInt32(dataSize.ToInt64());
+		this->data = gcnew array<Byte>(dataSize32);		
 		Marshal::Copy((IntPtr)dataPtr, this->data, 0, dataSize32);
 	}
 }
